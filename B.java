d6dd8501cdf56ddfd26a01007257e127182d4662
NickedCB <101660126+NickedCB@users.noreply.github.com>
2022-03-17 19:17:38 +0530

    Create B.java

---
+++ B.java
@@ -0,0 +1,20 @@
+public static void main(String[] args) {
+  
+  	for(int i=0; i <7; i++) {
+
+			for(int j=0; j<7; j++) {
+			
+				if((i==0 || i==3 || i==6 )&& (j!=6))
+					System.out.print("*");
+				
+				else if ((i!=0 && i!=3 && i!=6) && (j==0||j==6))	
+					System.out.print("*");
+				
+				else
+					System.out.print(" ");
+			}
+			System.out.println("");
+	}
+  
+  
+}
